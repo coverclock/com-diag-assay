cc6c293adbef628a11dc9d483ded1f0dc3dec2a4
Chip Overclock <coverclock@diag.com>
2022-02-07 10:12:24 -0700

    [1] Updated link options to recent Linux distro by removing -ly. [2] Bumped release. [3] All sanity unit tests PASS.

--- Assay/cfg/host.mk
+++ Assay/cfg/host.mk
@@ -17,5 +17,5 @@ LDARCH				:=	-L$(OUT)/$(LIB_DIR)
 MOARCH				:=	-L$(OUT)/$(LIB_DIR)
 SOARCH				:=
 KERNELARCH			:=
-LDLIBRARIES			:=	-lfl -ly
+LDLIBRARIES			:=	-lfl# -lfy
 #LDLIBRARIES			:=	/usr/lib/arm-linux-gnueabihf/libfl.a /usr/lib/arm-linux-gnueabihf/liby.a
